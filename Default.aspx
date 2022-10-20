971899259e2bb4a8e97eeb2a403ad36a2689022b
LinQU <asirayudha11@gmail.com>
2022-10-20 15:46:47 +0700

    first commit

---
+++ Default.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LatihanDatabase.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Manajemen Data Karyawan</title>
+    <link rel="stylesheet" href="Styles/Style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Data Karyawan</h2>
+            <hr />
+
+            <asp:Panel runat="server" ID="panelViewData">
+                <asp:LinkButton runat="server" ID="linkTambahBaru" OnClick="linkTambahBaru_Click">+ Tambah Baru</asp:LinkButton>
+                <br />
+
+                <label>Pencarian </label>
+                <asp:TextBox runat="server" ID="txtPencarian"></asp:TextBox>
+                <asp:Button runat="server" ID="btnPencarian" Text="Cari" OnClick="btnPencarian_Click" />
+                <br />
+
+                <asp:GridView runat="server" ID="gridData" PageSize="5" AllowPaging="true"
+                    AllowSorting="false" DataKeyNames="kry_id" ShowHeader="true"
+                    ShowHeaderWhenEmpty="true" EmptyDataText="Tidak ada data"
+                    AutoGenerateColumns="false" OnPageIndexChanging="gridData_PageIndexChanging" OnRowCommand="gridData_RowCommand">
+
+                    <PagerSettings Mode="NumericFirstLast" FirstPageText="<<" LastPageText=">>" />
+
+                    <Columns>
+                        <asp:BoundField DataField="rownum" HeaderText="No" ItemStyle-HorizontalAlign="Center" />
+                        <asp:BoundField DataField="kry_npk" HeaderText="NPK" ItemStyle-HorizontalAlign="Center" />
+                        <asp:BoundField DataField="kry_nama" HeaderText="Nama" />
+                        <asp:BoundField DataField="kry_provinsi" HeaderText="Provinsi" />
+                        <asp:TemplateField HeaderText="Aksi" ItemStyle-HorizontalAlign="Center">
+                            <ItemTemplate>
+                                <asp:LinkButton runat="server" ID="linkUbah" CommandName="Ubah" CommandArgument='<%# DataBinder.Eval(Container, "RowIndex") %>'><i class="bi bi-pencil-square"></i></asp:LinkButton>&nbsp;
+                                <asp:LinkButton runat="server" ID="linkHapus" CommandName="Hapus" CommandArgument='<%# DataBinder.Eval(Container, "RowIndex") %>'><i class="bi bi-trash"></i></asp:LinkButton>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </asp:Panel>
+
+            <asp:Panel runat="server" ID="panelManupulasiData" Visible="false">
+                <h2>
+                    <asp:Literal runat="server" ID="literalTitle"></asp:Literal>
+                </h2>
+                <asp:TextBox runat="server" ID="hiddenID" Visible="false"></asp:TextBox>
+
+                <label>NPK</label>
+                <br />
+                <asp:TextBox runat="server" ID="txtNPK"></asp:TextBox>
+                <br />
+                <br />
+
+                <label>Nama</label>
+                <br />
+                <asp:TextBox runat="server" ID="txtNama"></asp:TextBox>
+                <br />
+                <br />
+
+                <label>Provinsi</label>
+                <br />
+                <asp:DropDownList runat="server" ID="ddProvinsi"></asp:DropDownList>
+                <br />
+                <br />
+                <asp:Button runat="server" ID="btnUpdate" Text="Ubah" OnClick="btnUpdate_Click" />                
+                <asp:Button runat="server" ID="btnKirim" Text="Simpan" OnClick="btnKirim_Click" />
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
+using System.Web.UI.WebControls;
+
+namespace LatihanDatabase
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public string TempID = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                loadData(txtPencarian.Text);
+                getListProvinsi();
+            }
+            gridData.Width = Unit.Percentage(100);
+        }
+
+        protected void loadData(string query)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                conn.Open();
+                //VIEW DATA WITH STORE PRECODURE
+                SqlCommand cmd = new SqlCommand("viewKar", conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@query", query);
+                cmd.Parameters.AddWithValue("@prov", query);
+                
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gridData.DataSource = dt;
+                gridData.DataBind();
+                conn.Close();
+                
+
+               
+
+                gridData.DataSource = dt;
+                gridData.DataBind();
+                clearForm();
+                panelViewData.Visible = true;
+                panelManupulasiData.Visible = false;
+                btnKirim.Visible = true;
+                btnUpdate.Visible = false;
+                conn.Close();
+            }
+            catch { }
+        }
+
+        protected void getListProvinsi()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                conn.Open();
+
+                SqlCommand command =
+                    new SqlCommand("select pro_id, pro_nama from lat_msprovinsi order by pro_nama asc;", conn);
+
+                DataTable dt = new DataTable();
+                dt.Load(command.ExecuteReader());
+
+                ddProvinsi.Items.Clear();
+                ddProvinsi.DataSource = dt;
+                ddProvinsi.DataValueField = "pro_nama";
+                ddProvinsi.DataTextField = "pro_nama";
+                ddProvinsi.DataBind();
+                ddProvinsi.Items.Insert(0, new ListItem("-- Pilih Provinsi --", ""));
+                ddProvinsi.AppendDataBoundItems = false;
+
+                conn.Close();
+            }
+            catch { }
+        }
+
+        protected void btnPencarian_Click(object sender, EventArgs e)
+        {
+            loadData(txtPencarian.Text);
+        }
+
+        protected void linkTambahBaru_Click(object sender, EventArgs e)
+        {
+            clearForm();
+            panelViewData.Visible = false;
+            panelManupulasiData.Visible = true;
+            literalTitle.Text = "Form Tambah Data Karyawan";
+        }
+
+        protected void btnKirim_Click(object sender, EventArgs e)
+        {
+            if (hiddenID.Text.Equals(""))
+                createDataKaryawan();
+            else
+                updateDataKaryawan(hiddenID.Text);
+            panelViewData.Visible = true;
+            panelManupulasiData.Visible = false;
+        }
+
+        protected void createDataKaryawan()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                conn.Open();
+
+                //inser data use store procedure
+                SqlCommand command = new SqlCommand("insertKar", conn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@kry_npk", txtNPK.Text);
+                command.Parameters.AddWithValue("@kry_nama", txtNama.Text);
+                command.Parameters.AddWithValue("@kry_prov", ddProvinsi.Text);
+
+                command.ExecuteNonQuery();
+                
+
+
+                conn.Close();
+
+                loadData(txtPencarian.Text);
+            }
+            catch { }
+        }
+
+        protected void deleteDataKaryawan(string id)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                conn.Open();
+
+                //delete data with store procedure
+                SqlCommand command = new SqlCommand("deleteKar", conn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@id", id);
+
+                command.ExecuteNonQuery();
+                
+
+                conn.Close();
+
+                loadData(txtPencarian.Text);
+            }
+            catch { }
+        }
+
+        protected void updateDataKaryawan(string id)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                conn.Open();
+
+                //update data with store procedure
+                SqlCommand command = new SqlCommand("updateKar", conn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@kry_npk", txtNPK.Text);
+                command.Parameters.AddWithValue("@kry_nama", txtNama.Text);
+                command.Parameters.AddWithValue("@kry_prov", ddProvinsi.SelectedValue);
+
+                command.ExecuteNonQuery();
+                
+
+
+
+                conn.Close();
+
+                loadData(txtPencarian.Text);
+            }
+            catch { }
+        }
+
+        protected void clearForm()
+        {
+            hiddenID.Text = "";
+            txtNPK.Text = "";
+            txtNama.Text = "";
+            ddProvinsi.SelectedIndex = 0;
+            txtPencarian.Text = "";
+        }
+
+        protected void gridData_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridData.PageIndex = e.NewPageIndex;
+            loadData(txtPencarian.Text);
+        }
+
+        protected void gridData_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName != "Page")
+            {
+               string TempID = gridData.DataKeys[Convert.ToInt32(e.CommandArgument)].Value.ToString();
+
+                if (e.CommandName == "Hapus")
+                {
+                    deleteDataKaryawan(TempID);
+                }
+                else if (e.CommandName == "Ubah")
+                {
+                    hiddenID.Text = TempID;
+                    panelViewData.Visible = false;
+                    panelManupulasiData.Visible = true;
+                    literalTitle.Text = "Form Ubah Data Karyawan";
+
+                    try
+                    {
+                        SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                        conn.Open();
+
+                        SqlCommand command = new SqlCommand("select kry_npk, kry_nama, kry_provinsi from lat_mskaryawan where kry_id = '" + TempID + "';", conn);
+
+                        DataTable dt = new DataTable();
+                        dt.Load(command.ExecuteReader());
+
+                        txtNPK.Text = dt.Rows[0][0].ToString();
+                        txtNama.Text = dt.Rows[0][1].ToString();
+                        ddProvinsi.SelectedValue = dt.Rows[0][2].ToString();
+
+                        btnKirim.Visible = false;
+                        btnUpdate.Visible = true;
+                        
+
+                        conn.Close();
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            updateDataKaryawan(hiddenID.Text);
+            loadData(txtPencarian.Text);
+        }
+    }
+}
